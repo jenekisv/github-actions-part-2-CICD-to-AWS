2a0349f2d0960ff9715dbe536b3588665078f4c9
jenekisv <38710485+jenekisv@users.noreply.github.com>
2023-12-01 00:17:15 +0600

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,29 @@
-python
+# ------------------------------------------------
+# Program by Evgenii
+#
+# Version      Date           Info
+# 2.0          2023           Initial Version
+#
+# ----------------------------------------------
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+
+@application.route("/hello")
+def index():
+    return "Hello World from Flask Hello Page.<b> v1.0"
+
+#--------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+#------------------------------
